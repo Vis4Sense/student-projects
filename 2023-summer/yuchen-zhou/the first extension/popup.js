21fff15c6493dbd054e10bd15afd656757c16fd3
zouzou <44434997+zouzoudonotlikericecongee@users.noreply.github.com>
2023-08-12 14:18:35 +0100

    Add files via upload

---
+++ 2023-summer/yuchen-zhou/the first extension/popup.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var sidebarContent = document.getElementById('sidebar-content');
+    sidebarContent.textContent = 'hi KONGKONG';
+  });
+  
