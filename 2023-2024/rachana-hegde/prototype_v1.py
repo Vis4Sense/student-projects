745dd89318a7651380f5488da63b858da6d34c0a
Rachana Hegde <66835338+rachanahegde@users.noreply.github.com>
2023-11-28 11:32:19 +0000

    Update prototype_v1.py

--- 2023-2024/rachana-hegde/prototype_v1.py
+++ 2023-2024/rachana-hegde/prototype_v1.py
@@ -4,8 +4,11 @@ import requests
 import io
 from PIL import Image
 
+# Note: Don't share this!
+TOKEN = ""
+
 API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
-headers = {"Authorization": "Bearer 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"}
+headers = {"Authorization": f"Bearer {TOKEN}"}
 
 
 def query(payload):
@@ -20,4 +23,3 @@ image_bytes = query({
 # Sometimes this code returns an error and other times it works
 image = Image.open(io.BytesIO(image_bytes))
 image.show()  # Open image in your OS default image viewer
-
