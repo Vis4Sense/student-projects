6f33ed2e146edd415c81339a678030aa6794b957
scyjl14 <scyjl14@exmail.nottingham.ac.uk>
2023-12-27 16:54:23 +0000

    updated project code structure

---
+++ 2023-2024/jianwen-lyu/project-code/myextension/src/index.ts
@@ -0,0 +1,23 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import {ButtonExtension} from "./button";
+
+/**
+ * Initialization data for the myextension extension.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'myextension:plugin',
+  description: 'A JupyterLab extension.',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    console.log('JupyterLab extension myextension is activated!');
+    
+    let buttonExtension = new ButtonExtension();
+    app.docRegistry.addWidgetExtension('Notebook', buttonExtension);
+  }
+};
+
+export default extension;
