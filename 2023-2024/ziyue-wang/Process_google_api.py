bada33989df406fd8b2d93689fe5d8525d8e24df
Ziyue WANG <92834281+scyzw13@users.noreply.github.com>
2024-03-05 20:58:50 +0800

    Update Process_google_api.py

--- 2023-2024/ziyue-wang/Process_google_api.py
+++ 2023-2024/ziyue-wang/Process_google_api.py
@@ -3,6 +3,6 @@ for item in itemListElement:
     name = item['result']['name']
     description = item['result']['description']
     article_body = item['result']['detailedDescription']['articleBody']
-    # 将实体名称、描述和文章摘要整合为一个文本
+    # to make the names, description, and the article into a new text file
     combined_text = f"{name}. {description}. {article_body}"
     text_data.append(combined_text)
