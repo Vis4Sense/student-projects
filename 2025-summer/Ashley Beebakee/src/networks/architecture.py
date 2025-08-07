0e37cba1683db5f448c8beb6da6414f422a41e66
OmniAshley <omniashleybeebakee@gmail.com>
2025-08-07 15:48:39 +0100

    chore: renamed "architectures" folder to "networks" and "deep_learning.py" to "architecture.py"

--- 2025-summer/Ashley Beebakee/src/networks/architecture.py
+++ 2025-summer/Ashley Beebakee/src/networks/architecture.py
@@ -1,11 +1,11 @@
 #------------------------------------------------------------#
-# Name: Deep Learning Module
+# Name: Architecture Module
 # Description: This script defines deep learning architectures
 #              for training and evaluation of time series data
 #              fused with sentiment and technical indicators
 #              to predict future price movements.
 # Author: Ashley Beebakee (https://github.com/OmniAshley)
-# Last Updated: 23/07/2025
+# Last Updated: 07/08/2025
 # Python Version: 3.10.6
 # Packages Required: N/A
 #------------------------------------------------------------#
